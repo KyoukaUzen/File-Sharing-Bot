2d53ce7bc6096743007a32ee95e5627d0dfe5795
Kyouka Uzen <101930427+KyoukaUzen@users.noreply.github.com>
2022-07-14 20:20:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,8 +90,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("ɢʀᴏᴜᴘ", url=f"https://t.me/whatsappgang"),
+                    InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data = "close")
                 ]
             ]
         )
